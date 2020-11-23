842eb70dca2f3395d661cd0958853d45be18128d
Daniel Limas <amedlimas97@gmail.com>
2020-11-22 23:33:30 -0700

    Fixed port binding problem on deploy

--- server.js
+++ server.js
@@ -2,21 +2,15 @@ const express = require('express')
 const app = express()
 const serveIndex = require('serve-index');
 
-app.get("/app", (req, res) => {
-    res.send(console.log('Visualization of Sorting algorithms'))
-})
-
-app.listen(3000 || process.env.PORT, () => {
-    console.log(`Server listening at port 3000`)
-})
-
-app.use('/test', (req, res, next)=>{
-    console.log('Request type: ', req.method)
-    next()
-})
+app.set('port', process.env.PORT || 3000)
 
 app.use('/', express.static('public'))
 app.use('/', express.static('algorithms'))
 app.use('/', express.static('assets'))
 app.use('/', serveIndex('public'))
 
+app.listen(app.get('port'), () => {
+    console.log("App running on port: " + app.get('port'))
+})
+
+module.exports = app
